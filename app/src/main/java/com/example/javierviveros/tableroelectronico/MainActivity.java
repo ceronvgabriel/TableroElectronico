a3bcb758e10616d2929d76a7794f85957ca659f1
Gabriel <gaboceron10@hotmail.com>
2015-07-26 23:01:11 -0500

    Merge remote-tracking branch 'origin/Dialogos1' into Dialogos2
    Conflicts:
    	app/src/main/java/com/example/javierviveros/tableroelectronico/TextDialog.java
    	app/src/main/res/layout/fragment_text.xml
    	app/src/main/res/values/strings.xml

--- app/src/main/java/com/example/javierviveros/tableroelectronico/MainActivity.java
+++ app/src/main/java/com/example/javierviveros/tableroelectronico/MainActivity.java
@@ -1,24 +1,80 @@
 package com.example.javierviveros.tableroelectronico;
 
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public volatile boolean stopWorker;
+    public static boolean connected = false;
+    public static Button bascular;
+
+    private enlazarbt fragEnlazar = new enlazarbt();
+
+    private static ArrayList<String> BT_devices = new ArrayList<>();
+    public static ArrayList<String> getBT_devices(){
+        return BT_devices;
+    }
+
+    private BluetoothAdapter myBluetooth = null;
+    private static ArrayList<BluetoothDevice> btDeviceList = new ArrayList<BluetoothDevice>();
+    public static BluetoothDevice getBTdevice( int index ){
+        return btDeviceList.get(index);
+    }
+    private static BluetoothSocket clientSocket;
+    public static BluetoothSocket getsocket(){
+        return clientSocket;
+    }
+    public static void setsocket(BluetoothSocket ext){
+        clientSocket = ext;
+    }
+    private BroadcastReceiver discoveryMonitor = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Añadimos el dispositivo encontrado al adaptador del ListView.
+            //String remoteName = intent.getStringExtra(BluetoothDevice.EXTRA_NAME);
+            //BT_devices.add(remoteName);
+            // Recuperamos el dispositivo detectado y lo guardamos en el array de dispositivos.
+            BluetoothDevice remoteDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            btDeviceList.add(remoteDevice);
+            BT_devices.add(remoteDevice.getName()+" ("+remoteDevice.getAddress()+")");
+            fragEnlazar.actualizarContenido();
+        }
+    };
 
     private String[] velocids;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_main);
 
         velocids = getResources().getStringArray(R.array.speedItems);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(discoveryMonitor, new IntentFilter(BluetoothDevice.ACTION_FOUND));
     }
 
 
@@ -48,7 +104,12 @@ public class MainActivity extends ActionBarActivity {
         FragmentManager fragmentManager = getSupportFragmentManager();
 
         TextDialog speed = new TextDialog();
-                speed.show(fragmentManager, "DigiText");
+        speed.show(fragmentManager, "DigiText");
+    }
+
+    public void funConect(View v) {
+        EnlazarMenuItem();
+
     }
 
     public void selectDirecc(View v) {
@@ -68,4 +129,69 @@ public class MainActivity extends ActionBarActivity {
     private void msgToast(String s){
         Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
     }
+
+    private void desconectarBluetooth() {
+        if (clientSocket!=null) //If the btSocket is busy
+        {
+            try{
+                clientSocket.close(); //close connection
+            }catch (IOException e){
+                msg("Error");
+            }
+        }
+    }
+
+    private void EnlazarMenuItem() {
+        myBluetooth = BluetoothAdapter.getDefaultAdapter();
+        if( myBluetooth == null){
+            Toast.makeText(this, "Este dispositivo Android NO tiene Bluetooth", Toast.LENGTH_LONG).show();
+        }else{
+            if( !myBluetooth.isEnabled() ){
+                //Ask to the user turn the bluetooth on
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon, 1);
+            }else {
+                desconectarBluetooth();
+                discoverBTDevices();
+                getFragmentManager().beginTransaction().replace(android.R.id.content, fragEnlazar).commit();
+            }
+        }
+    }
+
+    private void discoverBTDevices(){
+        BT_devices.clear();
+        btDeviceList.clear();
+        myBluetooth.startDiscovery();
+    }
+
+    private void HelpMenuItem() {
+        new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.sHelp))
+                .setMessage(getResources().getString(R.string.sHelpInfo))
+                .setNeutralButton(getResources().getString(R.string.sOk),new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
+    }
+
+    private void AboutMenuItem() {
+        new AlertDialog.Builder(this)
+                .setTitle(getResources().getString(R.string.sAbout))
+                .setMessage(getResources().getString(R.string.sAboutInfo))
+                .setNeutralButton(getResources().getString(R.string.sOk),new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .show();
+    }
+
+    private void msg(String s){
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
 }
